41906a2060285bab34832d8017111a4914c3f6d0
jaes <dlwo9503@naver.com>
2021-07-28 10:51:07 +0900

    Kanban #2

--- 02.component/kanban/src/ex01/KanbanBoard.js
+++ 02.component/kanban/src/ex01/KanbanBoard.js
@@ -1,23 +1,13 @@
 import React, { Fragment } from 'react';
 import CardList from './CardList';
-import cards from './data.json'; // json은 그냥 import 가능
+import cards from './data.json';
 
-export default function KanbanBoard(){
-    
-    const cardList = {
-        ToDo: [],
-        Doing: [],
-        Done: []
-    };
-
-    cards.forEach(function(card){
-        cardList[card.status].push(card);
-    })
+export default function KanbanBoard() {
     return (
         <div className={'KanbanBoard'}>
-            <CardList key='ToDo' title={'ToDo'} cards={cards.filter(card => card.status=='ToDo')}/>
-            <CardList key='Doing' title={'Doing'} ccards={cards.filter(card => card.status=='Doing')}/>
-            <CardList key='Done' title={'Done'} cards={cards.filter(card => card.status=='Done')}/>
+            <CardList key="ToDo" title={'ToDo'} cards={cards.filter(card => card.status=='ToDo')}/>
+            <CardList key="Doing" title={'Doing'} cards={cards.filter(card => card.status=='Doing')}/>
+            <CardList key="Done" title={'Done'} cards={cards.filter(card => card.status=='Done')}/>
         </div>
     );
 }
