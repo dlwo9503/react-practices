68b8b0be7da24520a080577d283884e6f3d3f888
jaes <dlwo9503@naver.com>
2021-07-29 14:59:21 +0900

    Guestbook #css 및 scss 적용

--- 02.component/guestbook/src/Guestbook.js
+++ 02.component/guestbook/src/Guestbook.js
@@ -2,7 +2,7 @@ import React from 'react';
 import WriteForm from './WriteForm';
 import MessageList from './MessageList';
 import messages from './assets/json/messages.json';
-import styles from './assets/css/Guestbook.css';
+import styles from './assets/scss/Guestbook.scss';
 
 export default function Guestbook() {
     return (
