a7fcfd4a1d17b1290c43a5e116e4c0c3756de231
jaes <dlwo9503@naver.com>
2021-07-29 10:54:44 +0900

    webpack5 설정 76284a1

--- 01.basics/webpack-practices/ex04/webpack.config.js
+++ 01.basics/webpack-practices/ex04/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+    mode: "development",
     entry: path.resolve('src/index.js'),
     output: {
         path: path.resolve('public'),
@@ -10,11 +10,13 @@ module.exports = {
     module: {
         rules: [{
             test: /\.css$/i,
-            use: ['style-loader', 'css-loader']
+            use:['style-loader', 'css-loader']
         }]
     },
+    devtool: "eval-source-map",
     devServer: {
         contentBase: path.resolve('public'),
+        watchContentBase: true,
         host: "0.0.0.0",
         port: 9999,
         inline: true,
@@ -22,17 +24,5 @@ module.exports = {
         hot: false,
         compress: true,
         historyApiFallback: true
-    },
-    devtool: "eval-source-map",
-    devServer: {
-      contentBase: path.resolve("public"),
-      watchContentBase: true,
-      host: "0.0.0.0",
-      port: 9999,
-      inline: true,
-      liveReload: true,
-      hot: false,
-      compress: true,
-      historyApiFallback: true,
     }
 }
--- 01.basics/webpack-practices/ex05/webpack.config.js
+++ 01.basics/webpack-practices/ex05/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: path.resolve('src/index.js'),
     output: {
         path: path.resolve('public'),
@@ -9,19 +10,19 @@ module.exports = {
     module: {
         rules: [{
             test: /\.(sa|sc|c)ss$/i,
-            use: ['style-loader', 'css-loader', 'sass-loader']
+            use:['style-loader', 'css-loader', 'sass-loader']
         }]
     },
     devtool: "eval-source-map",
     devServer: {
-      contentBase: path.resolve("public"),
-      watchContentBase: true,
-      host: "0.0.0.0",
-      port: 9999,
-      inline: true,
-      liveReload: true,
-      hot: false,
-      compress: true,
-      historyApiFallback: true,
+        contentBase: path.resolve('public'),
+        watchContentBase: true,
+        host: "0.0.0.0",
+        port: 9999,
+        inline: true,
+        liveReload: true,
+        hot: false,
+        compress: true,
+        historyApiFallback: true
     }
 }
