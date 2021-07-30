bd2abdc718cc059524871a2c3e08e1234ea48bf4
jaes <dlwo9503@naver.com>
2021-07-30 15:34:15 +0900

    Guestbook #scroll1

--- 02.component/guestbook/src/App.js
+++ 02.component/guestbook/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useRef }from 'react';
 import Guestbook from './Guestbook';
 import './assets/css/App.css'
 
 export default function App() {
+    const outterRef = useRef(null);
+    const innerRef = useRef(null);
+
+    const onScroll = (e) => {
+        if(outterRef.current.scrollTop + outterRef.current.clientHeight >= innerRef.current.clientHeight) {
+            console.log("fetch");
+        }
+    };
+    
     return (
-        <Guestbook />
+        <div
+            ref={ ref => outterRef.current = ref } 
+            className='App'
+            onScroll={ onScroll }>
+            <div ref={ ref => innerRef.current = ref }>
+                <Guestbook />
+            </div>
+        </div>
     );
 }
