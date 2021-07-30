0908d54c471ba9c4540f6f6aaf2b55545515f273
jaes <dlwo9503@naver.com>
2021-07-30 12:06:38 +0900

    state#1

---
+++ 02.component/ex04/src/01/Incrementor02.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+export default function({begin, step}) {
+    const [ value, setValue ] = useState(begin);
+
+    const onClickButton = function() {
+        setValue(value + step);
+    }
+
+    return (
+        <div>
+            <button onClick={ onClickButton }>
+                <strong>+</strong>
+            </button>
+            { ' ' }
+            <span>{ value }</span>
+            { ' ' }
+            <button onClick={ onClickButton }>
+                <strong>-</strong>
+            </button>
+        </div>
+    )
+}
