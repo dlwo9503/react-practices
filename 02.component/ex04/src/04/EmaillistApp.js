9784a938807442fcea9d7d03bae651577eac2679
jaes <dlwo9503@naver.com>
2021-08-05 10:55:54 +0900

    stateful component & callback pattern #2

--- 02.component/ex04/src/04/EmaillistApp.js
+++ 02.component/ex04/src/04/EmaillistApp.js
@@ -5,10 +5,19 @@ import data from './assets/json/data.json';
 
 export default function EmaillistApp() {
     const [emails] =  useState(data);
+    const [keyword, setKeyword] = useState('');
+    const notifyKeywordChanged = function(keyword) {
+        setKeyword(keyword);
+    }
+
     return (
         <div className={ 'EmaillistApp' }>
-            <SearchBar />
-            <Emaillist emails={ emails }/>
+            <SearchBar 
+                callback={ notifyKeywordChanged }
+                keyword={ keyword }/>
+            <Emaillist 
+                emails={ emails }
+                keyword={ keyword }/>
         </div>
     );
 }
