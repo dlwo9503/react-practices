3a82bc9615483d28b2a83a46bc2b6fb32dc13aea
jaes <dlwo9503@naver.com>
2021-08-06 10:57:28 +0900

    lifecycle #4

--- 02.component/ex05/src/02/App.js
+++ 02.component/ex05/src/02/App.js
@@ -1,19 +1,16 @@
 import React, { Fragment, useState } from 'react';
-import LifeCycle from './LifeCycle';
+import Hook from './Hook';
 
 export default function App() {
     const [color, setColor] = useState('#000');
-
-    const handleClick = function () {
-        setColor(`#${Math.floor(Math.random() * 0xffffff).toString(16)}`);
-    }
-
+    
     return (
         <Fragment>
             <h2>ex05: Hook of Functional Component</h2>
-            <button onClick={handleClick}>색상변경</button>
-            <br />
-            <LifeCycle color={color} />
+            <button
+                onClick={ () => setColor(`#${Math.floor(Math.random() * 16777215).toString(16)}`) }>색상변경</button>
+            <br/>
+            <Hook color={color}/>
         </Fragment>
     );
 }
