47ffd01c6cf3dc20a34f211d4609294444793141
jaes <dlwo9503@naver.com>
2021-08-09 12:54:11 +0900

    app01.emaillist #2

---
+++ 07.Integration/app01.Emaillist/backend/models/emaillist.js
@@ -0,0 +1,34 @@
+// 이메일리스트 모델
+const util = require('util');
+const dbconn = require('./dbconn');
+
+module.exports = {
+    findAll: async function() {
+        const conn = dbconn();
+        // const query = (sql, data) => new Promise((resolve, reject) => conn.query(sql, data, (error, rows, field) => error ? reject(error):resolve(rows))); 
+        const query = util.promisify(conn.query).bind(conn);
+
+        try {
+            const results = await query("select first_name as firstName, last_name as lastName, email from emaillist order by no desc", []);
+            return results;    
+        } catch(e) {
+            console.error(e);
+        } finally {
+            conn.end();
+        }
+    },
+    insert: async function(emaillist) {
+        console.log(emaillist);
+        console.log(Object.values(emaillist)); // 객체를 배열로 만들어 줌
+        const conn = dbconn();
+        const query = util.promisify(conn.query).bind(conn);
+
+        try {
+            return await query("insert into emaillist values(null, ?, ?, ?)", Object.values(emaillist));
+        } catch(e) {
+            console.error(e);
+        } finally {
+            conn.end();
+        }
+    }
+}
