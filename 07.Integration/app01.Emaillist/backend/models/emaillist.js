334d42f30e7cc56f941ace87a23aeed300ba3789
jaes <dlwo9503@naver.com>
2021-08-09 16:38:19 +0900

    app01.emaillist #finish

--- 07.Integration/app01.Emaillist/backend/models/emaillist.js
+++ 07.Integration/app01.Emaillist/backend/models/emaillist.js
@@ -9,7 +9,7 @@ module.exports = {
         const query = util.promisify(conn.query).bind(conn);
 
         try {
-            const results = await query("select first_name as firstName, last_name as lastName, email from emaillist order by no desc", []);
+            const results = await query("select no, first_name as firstName, last_name as lastName, email from emaillist order by no desc", []);
             return results;    
         } catch(e) {
             console.error(e);
